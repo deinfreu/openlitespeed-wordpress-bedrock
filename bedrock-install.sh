c3306ced8d6706d6ab9f82266a485eb9bae1d5c5
Daniel Freudenberg <freudend@gmail.com>
2022-10-04 23:31:52 +0200

    Git clone bedrock repository

--- bedrock-install.sh
+++ bedrock-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #updates packages
-cd ~
 sudo apt update
 
 #Install composer
@@ -11,12 +10,6 @@ curl -s https://getcomposer.org/installer -o /tmp/composer-setup.php
 #Install composer globally
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-sudo chown -R freudend /home/freudend/.config/composer
-
-#Grants permission to /var/www/html 
-sudo chown -R freudend /var/www/html
-
 #Install bedrock in /var/www/html
-cd ~
-cd /var/www/html
-sudo composer create-project roots/bedrock
+cd /var/www
+sudo git clone https://github.com/roots/bedrock.git
