fa2a31ba1eb1cc4fe42c4e204735c51f9662f290
Daniel Freudenberg <freudend@gmail.com>
2022-10-04 23:00:39 +0200

    fix

--- bedrock-install.sh
+++ bedrock-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #updates packages
+cd ~
 sudo apt update
 
 #Install composer
 cd ~
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+curl -s https://getcomposer.org/installer -o /tmp/composer-setup.php
 
 #Install composer globally
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
