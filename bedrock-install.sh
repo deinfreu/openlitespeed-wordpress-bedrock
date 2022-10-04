bf16b838c39901fab5b5d677625ae81304077c57
Daniel Freudenberg <freudend@gmail.com>
2022-10-04 23:09:59 +0200

    fix composer install warning

--- bedrock-install.sh
+++ bedrock-install.sh
@@ -11,6 +11,9 @@ curl -s https://getcomposer.org/installer -o /tmp/composer-setup.php
 #Install composer globally
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
+#Grants permission to /var/www/html 
+sudo chown -R freudend /var/www/html
+
 #Install bedrock in /var/www/html
 cd ~
 cd /var/www/html
