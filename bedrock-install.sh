11a2344f20a240d66d73704be0fddad4eeb17172
Daniel Freudenberg <freudend@gmail.com>
2022-10-04 22:40:00 +0200

    Installs bedrock

--- bedrock-install.sh
+++ bedrock-install.sh
@@ -7,15 +7,13 @@ sudo apt update
 cd ~
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 
-#Checks if hash of the installed composer is the same as the version on the verified server
-HASH=`curl -sS https://composer.github.io/installer.sig`
-echo $HASH
-
-#Verify installation 
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-
 #Install composer globally
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 #Test composer
 composer
+
+#Install bedrock in /var/www/html
+cd /var/www/html
+sudo composer create-project roots/bedrock
+yes
